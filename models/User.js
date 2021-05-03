72a4df23b4964bd57945723f2b1a4f0ebe317099
alex-146 <deynekos1488@gmail.com>
2021-05-03 22:31:00 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,11 +2,27 @@ const Sequelize = require("sequelize");
 const db = require("../config/database");
 
 const User = db.define("User", {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: Sequelize.INTEGER
+  },
   name: {
+    allowNull: false,
     type: Sequelize.STRING
   },
   age: {
-    type: Sequelize.SMALLINT
+    allowNull: false,
+    type: Sequelize.INTEGER
+  },
+  createdAt: {
+    allowNull: false,
+    type: Sequelize.DATE
+  },
+  updatedAt: {
+    allowNull: false,
+    type: Sequelize.DATE
   }
 });
 
